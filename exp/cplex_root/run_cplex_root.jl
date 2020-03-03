4e44699568a00217c508733ce81089aba90bcfd0
Mathieu Tanneau <mathieu.tanneau@gmail.com>
2020-03-03 10:07:50 -0500

    Enable bridges and user-defined cut passes

--- exp/cplex_root/run_cplex_root.jl
+++ exp/cplex_root/run_cplex_root.jl
@@ -37,11 +37,12 @@ micp_optimizer = MOI.OptimizerWithAttributes(CPLEX.Optimizer,
     "CPXPARAM_MIP_Strategy_HeuristicFreq" => -1     # Disable heuristics
 )
 
-sf = build_standard_form(micp_optimizer, cbf)
+sf = build_standard_form(micp_optimizer, cbf, bridge_type=Float64)
 
 # Solve root node with aggressive L-a-P cuts
 micp = sf.model
-MOI.set(micp, MOI.RawParameter("CPXPARAM_MIP_Limits_CutPasses"), ARGS[2])
+nrounds = parse(Int, ARGS[2])
+MOI.set(micp, MOI.RawParameter("CPXPARAM_MIP_Limits_CutPasses"), nrounds)
 MOI.set(micp, MOI.RawParameter("CPXPARAM_MIP_Limits_CutsFactor"), 1e30)
 MOI.set(micp, MOI.RawParameter("CPXPARAM_MIP_Cuts_LiftProj"), 3)
 
