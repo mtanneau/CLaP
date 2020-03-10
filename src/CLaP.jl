bb55133c3e426f4827af06a9fc9263f6a5c9f448
Mathieu Tanneau <mathieu.tanneau@gmail.com>
2020-03-10 09:41:53 -0400

    Export strengthen

--- src/CLaP.jl
+++ src/CLaP.jl
@@ -9,7 +9,8 @@ export MOI,
     NONLINEAR_CONE, POLYHEDRAL_CONE, MOI_CONES, SCALAR_SETS, SUPPORTED_SETS,
     StandardProblem, build_standard_form,
     extract_implied_integer,
-    build_cgcp
+    build_cgcp,
+    strengthen
 
 
 include("standard_form.jl")
