d4af272cdec80e882dfe88c5eaf24a35e29ec2ef
mtanneau <mathieu.tanneau@gmail.com>
2020-07-23 11:23:02 -0400

    Add info callback to track CPLEX progress

--- exp/cplex_root/run_cplex_root.jl
+++ exp/cplex_root/run_cplex_root.jl
@@ -1,10 +1,10 @@
 using LinearAlgebra
 using SparseArrays
+using Logging
 
 import CPLEX
 
-include(joinpath(@__DIR__, "../../src/CLaP.jl"))
-using .CLaP
+using CLaP
 
 const CUT_PARAMS = [
     "CPXPARAM_MIP_Cuts_BQP",
@@ -24,33 +24,64 @@ const CUT_PARAMS = [
     "CPXPARAM_MIP_Cuts_ZeroHalfCut"
 ]
 
-# Read model from file
-cbf = MOI.FileFormats.CBF.Model()
-MOI.read_from_file(cbf, ARGS[1])
+function main(finst, maxrounds=200, maxtime=3600.0; verbose::Bool=true)
+    # Read model from file
+    cbf = MOI.FileFormats.CBF.Model()
+    MOI.read_from_file(cbf, finst)
 
-# Create standard form problem
-micp_optimizer = MOI.OptimizerWithAttributes(CPLEX.Optimizer,
-    "CPX_PARAM_PREIND" => 0,                        # Disable presolve (0)
-    "CPX_PARAM_THREADS" => 1,                       # Single thread
-    "CPXPARAM_MIP_Strategy_MIQCPStrat" => 2,        # 1: NL-B&B, 2:OA
-    [cutparam => -1 for cutparam in CUT_PARAMS]..., # Disable all cuts
-    "CPXPARAM_MIP_Limits_Nodes" => 0,               # Root node only
-    "CPXPARAM_MIP_Strategy_HeuristicFreq" => -1     # Disable heuristics
-)
+    # Create standard form problem
+    micp_optimizer = MOI.OptimizerWithAttributes(CPLEX.Optimizer,
+        "CPX_PARAM_PREIND" => 0,                        # Disable presolve (0)
+        "CPX_PARAM_THREADS" => 1,                       # Single thread
+        "CPXPARAM_MIP_Strategy_MIQCPStrat" => 2,        # 1: NL-B&B, 2:OA
+        [cutparam => -1 for cutparam in CUT_PARAMS]..., # Disable all cuts
+        "CPXPARAM_MIP_Limits_Nodes" => 0,               # Root node only
+        "CPXPARAM_MIP_Strategy_HeuristicFreq" => -1     # Disable heuristics
+    )
 
-sf = build_standard_form(micp_optimizer, cbf, bridge_type=Float64)
+    sf = build_standard_form(micp_optimizer, cbf, bridge_type=Float64)
 
-# Solve root node with aggressive L-a-P cuts
-micp = sf.model
-nrounds = parse(Int, ARGS[2])
-MOI.set(micp, MOI.RawParameter("CPXPARAM_MIP_Limits_CutPasses"), nrounds)
-MOI.set(micp, MOI.RawParameter("CPXPARAM_MIP_Limits_CutsFactor"), 1e30)
-MOI.set(micp, MOI.RawParameter("CPXPARAM_MIP_Cuts_LiftProj"), 3)
+    # Solve root node with aggressive L-a-P cuts
+    micp = sf.model
+    MOI.set(micp, MOI.RawParameter("CPXPARAM_MIP_Limits_CutPasses"), maxrounds)
+    MOI.set(micp, MOI.RawParameter("CPXPARAM_MIP_Limits_CutsFactor"), 1e30)
+    MOI.set(micp, MOI.RawParameter("CPXPARAM_MIP_Cuts_LiftProj"), 3)
 
-# Time limit
-MOI.set(micp, MOI.TimeLimitSec(), 3600.0)  # 1-hour time limit
+    # Time limit
+    MOI.set(micp, MOI.Silent(), !verbose)
+    MOI.set(micp, MOI.TimeLimitSec(), maxtime)  # 1-hour time limit
 
-MOI.optimize!(micp)
+    # Callback for tracking bound progress
+    tstart = time()
+    ncalls = 0
+    function cbinfo(cbdata)
+        x_ = MOI.get(micp, MOI.CallbackVariablePrimal(cbdata), sf.var_indices)
+        z_ = dot(sf.c, x_)
+        ncalls += 1
+        t_ = time() - tstart
+        @info "Current stats" z_ t_ ncalls
+        return nothing
+    end
 
-@show MOI.get(micp, MOI.TerminationStatus())    # Termination status
-@show MOI.get(micp, MOI.ObjectiveBound())       # Lower bound
+    MOI.set(micp, MOI.UserCutCallback(), cbinfo)
+
+    tstart = time()
+    MOI.optimize!(micp)
+
+    verbose && @show MOI.get(micp, MOI.TerminationStatus())    # Termination status
+    verbose && @show MOI.get(micp, MOI.ObjectiveBound())       # Lower bound
+
+    return micp
+end
+
+if abspath(PROGRAM_FILE) == @__FILE__
+    nrounds = parse(Int, ARGS[2])
+    maxtime = parse(Float64, ARGS[3])
+    
+    with_logger(Logging.NullLogger()) do
+        main(ARGS[1], 2, 5.0, verbose=false)
+    end
+
+    
+    main(ARGS[1], nrounds, maxtime)
+end
